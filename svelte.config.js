368b4b637ac88ab79034be735c75b825eae30ad6
TheLeonStyle <kirzim2007@gmail.com>
2024-11-06 07:15:29 +0400

    ocen

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
+
+// import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,16 +10,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true,
-			paths: {
-				base: '/theleonstyle.github.io'
-			}
-		})
+		adapter: adapter(),
+		target: '#svelte' // целевой элемент для приложения
 	},
 	preprocess: vitePreprocess()
 };
